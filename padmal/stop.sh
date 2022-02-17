ef27ffa6c1724652dd8cbe0c34d857ef06497162
Padmal <CloudyPadmal@users.noreply.github.com>
2022-02-17 17:45:06 +0100

    fix: stop script to run on mac

--- padmal/stop.sh
+++ padmal/stop.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-PATTERN='python3 serial-log.py'
+YELOW='\033[1;33m'
+LBLUE='\033[1;34m'
+REDDD='\033[0;31m'
+GREEN='\033[0;32m'
+CLEAR='\033[0m'
+PATTERN='serial-log.py'
 
-status=$(pgrep -f "${PATTERN}")
+PROCESS_IDS=$(pgrep -f ${PATTERN})
 
-for i in $status
+for i in $(echo "$PROCESS_IDS" | tr "\n" " ")
 do
-   kill -6 "$i"
+  COMD=$(ps -p "$i" -o command | awk '{print $3}' | tr "\n" " ")
+  echo -e "${YELOW}Stopping ${REDDD}${i}${CLEAR} -${LBLUE}${COMD}${CLEAR}"
+  kill -6 "$i"
 done
 
-cd Log
+echo -e "\n${GREEN}All logging processes have been stopped successfully!\n${CLEAR}"
+
+cd Log || exit
 python3 plot.py
