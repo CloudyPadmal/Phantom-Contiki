d3bbb42268f8c2dd3722c726086f096efa2fc5a1
Padmal <CloudyPadmal@users.noreply.github.com>
2022-02-19 23:07:51 +0100

    feat: serial handling of transmitter ops
    Signed-off-by: Padmal <CloudyPadmal@users.noreply.github.com>

--- padmal/serial-log.py
+++ padmal/serial-log.py
@@ -4,17 +4,24 @@ import serial as s
 import sys
 import signal
 
-if len(sys.argv) is not 3:
-    print("Port and Log missing")
+Transmitter = True
+Receiver = False
+
+mode = Transmitter
+if len(sys.argv) < 2:
+    print("Port is missing")
     exit()
+
+if len(sys.argv) == 3:
+    mode = Receiver
+    file = open(sys.argv[2], 'w')
 
 ser = None
 try:
     ser = s.Serial(sys.argv[1], baudrate=115200)
 except s.SerialException as e:
-    print("\033[91mOperation failed for\033[1m", (sys.argv[1]).split("/")[1], "\033[0m")
+    print("\033[91mOperation failed for\033[1m", (sys.argv[1]).split("/")[2], "\033[0m")
     exit()
-file = open(sys.argv[2], 'w')
 
 
 def handler(signum, frame):
@@ -29,22 +36,31 @@ signal.signal(signal.SIGABRT, handler)
 # ser.write(b'r\n')
 # ser.write(b'w\n')
 # ser.write(b'\n')
-ser.write(b'R\n')
+if mode == Transmitter:  # Reset packet counter and start transmission
+    ser.write(b'R\n')
 
-count = 0
+attempted = False
 
 while True:
     try:
-        line = ser.readline()
-        if line:
-            L = line.decode('utf-8', errors='ignore')
-            file.write(L)
-    except Exception as e:
+        if Receiver:
+            line = ser.readline()
+            if line:
+                L = line.decode('utf-8', errors='ignore')
+                file.write(L)
+        else:
+            command = input("Command (r,R,w,s,q): ")
+            if command == 'q':
+                ser.close()
+                exit()
+            ser.write(command.encode() + b'\n')
+    except Exception as e:  # Sometimes serial is not setup, so we retry once
         print("Error: {0}".format(e))
         ser = s.Serial(sys.argv[1], baudrate=115200)
-        if count == 1:
+        if attempted:
             file.close()
             ser.close()
             exit()
         else:
-            count = count + 1
+            attempted = True
+
