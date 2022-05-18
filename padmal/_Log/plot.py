c1f0e53cb1aaaa946c36f2d1dc72499875c35f5b
Padmal <CloudyPadmal@users.noreply.github.com>
2022-05-18 14:05:16 +0200

    refactor: reduced number of code lines used in plot.py
    Signed-off-by: Padmal <CloudyPadmal@users.noreply.github.com>

--- padmal/_Log/plot.py
+++ padmal/_Log/plot.py
@@ -9,14 +9,20 @@ PACKETS = int(input("PACKETS: ") or "2500")
 
 MIN_RSSI = -100
 MAX_RSSI = -10
-BINS = [i * (PACKETS / 10) for i in range(11)]
+BIN_PORTION = PACKETS / 10
+BINS = [i * BIN_PORTION for i in range(11)]
 
 SCATTER = 5
 L_WIDTH = 0.5
 
 props = dict(boxstyle='round', facecolor='#cccccc', alpha=0.5)
 
-TX_NODE = '5a7a.b713.0074.1200'
+TX_NODE = '67b5.5e11.0074.1200'
+
+ttl = 'RSSI Measurements {Total Packet Count: ' \
+      + str(PACKETS) + '; Transmit Power: ' \
+      + str(POWER_L) + ' dBm; Packet Rate: ' \
+      + str(RATE_P) + ' ms; Packets per Bin: ' + str(int(BIN_PORTION)) + ' packets}'
 
 
 def extract_packet_data(filename):
@@ -57,100 +63,54 @@ Ev4 = open('Eaves-4.txt', 'r')
 Ph2 = open('Receive.txt', 'r')
 
 #######################################################################################################################
-# Plots                                                                                                               #
+# Plot preparation                                                                                                            #
 #######################################################################################################################
 f, ((ev1, ev2, ev3, ev4, pha), (fr1, fr2, fr3, fr4, fr5)) = plt.subplots(2, 5)
 
-ttl = 'RSSI Measurements {Packets: ' + str(PACKETS) + '; Power: ' + str(POWER_L) + ' dBm; Rate: ' + str(RATE_P) + \
-      ' ms; Bin: ' + str(int(PACKETS / 10)) + ' packets}'
+eAxes = [ev1, ev2, ev3, ev4, pha]
+fAxes = [fr1, fr2, fr3, fr4, fr5]
+
 f.suptitle(ttl, fontweight='bold')
+
 (P1_E1, S_P1_E1) = extract_packet_data(Ev1)
-prr1 = 'PRR:' + str(round((len(P1_E1) / PACKETS), 3) * 100)[:4] + '%'
-ev1.scatter(S_P1_E1, P1_E1, s=SCATTER, label='from node 1')
-ev1.plot([np.mean(P1_E1) for _ in range(PACKETS)], label='node 1 mean', linewidth=L_WIDTH)
-ev1.set_xlim(0, PACKETS)
-ev1.set_ylim(MIN_RSSI, MAX_RSSI)
-ev1.set_title('Eaves 01')
-ev1.set_xlabel('Sequence number')
-ev1.set_ylabel('RSSI (dBm)')
-ev1.text(0.3, 0.95, prr1, transform=ev1.transAxes, fontsize=8,
-         verticalalignment='center', bbox=props)
-print("EV 1 Ready")
-
 (P1_E2, S_P1_E2) = extract_packet_data(Ev2)
-prr2 = 'PRR:' + str(round((len(P1_E2) / PACKETS), 3) * 100)[:4] + '%'
-ev2.scatter(S_P1_E2, P1_E2, s=SCATTER, label='from node 1')
-ev2.plot([np.mean(P1_E2) for _ in range(PACKETS)], label='node 1 mean', linewidth=L_WIDTH)
-ev2.set_xlim(0, PACKETS)
-ev2.set_ylim(MIN_RSSI, MAX_RSSI)
-ev2.set_title('Eaves 02')
-ev2.set_xlabel('Sequence number')
-ev2.text(0.4, 0.95, prr2, transform=ev2.transAxes, fontsize=8,
-         verticalalignment='center', bbox=props)
-print("EV 2 Ready")
-
 (P1_E3, S_P1_E3) = extract_packet_data(Ev3)
-prr3 = 'PRR:' + str(round((len(P1_E3) / PACKETS), 3) * 100)[:4] + '%'
-ev3.scatter(S_P1_E3, P1_E3, s=SCATTER, label='from node 1')
-ev3.plot([np.mean(P1_E3) for _ in range(PACKETS)], label='node 1 mean', linewidth=L_WIDTH)
-ev3.set_xlim(0, PACKETS)
-ev3.set_ylim(MIN_RSSI, MAX_RSSI)
-ev3.set_title('Eaves 03')
-ev3.set_xlabel('Sequence number')
-ev3.text(0.4, 0.95, prr3, transform=ev3.transAxes, fontsize=8,
-         verticalalignment='center', bbox=props)
-print("EV 3 Ready")
-
 (P1_E4, S_P1_E4) = extract_packet_data(Ev4)
-prr4 = 'PRR:' + str(round((len(P1_E4) / PACKETS), 3) * 100)[:4] + '%'
-ev4.scatter(S_P1_E4, P1_E4, s=SCATTER, label='from node 1')
-ev4.plot([np.mean(P1_E4) for _ in range(PACKETS)], label='node 1 mean', linewidth=L_WIDTH)
-ev4.set_xlim(0, PACKETS)
-ev4.set_ylim(MIN_RSSI, MAX_RSSI)
-ev4.set_title('Eaves 04')
-ev4.set_xlabel('Sequence number')
-ev4.text(0.4, 0.95, prr4, transform=ev4.transAxes, fontsize=8,
-         verticalalignment='center', bbox=props)
-print("EV 4 Ready")
-
 (P1_P2, S_P1_P2) = extract_packet_data(Ph2)
-prr5 = 'PRR:' + str(round((len(P1_P2) / PACKETS), 3) * 100)[:4] + '%'
-pha.scatter(S_P1_P2, P1_P2, s=SCATTER, label='from node 1')
-pha.plot([np.mean(P1_P2) for _ in range(PACKETS)], label='node 1 mean', linewidth=L_WIDTH)
-pha.set_xlim(0, PACKETS)
-pha.set_ylim(MIN_RSSI, MAX_RSSI)
-pha.set_title('In-body')
-pha.set_xlabel('Sequence number')
-pha.text(0.4, 0.95, prr5, transform=pha.transAxes, fontsize=8,
-         verticalalignment='center', bbox=props)
-print("RX Ready")
+
+PacketList = [P1_E1, P1_E2, P1_E3, P1_E4, P1_P2]
+SequenceList = [S_P1_E1, S_P1_E2, S_P1_E3, S_P1_E4, S_P1_P2]
 
 #######################################################################################################################
-# Fast RSSI Sampling                                                                                                  #
+# Plots                                                                                                  #
 #######################################################################################################################
-fr1.hist(S_P1_E1, BINS, label='count', alpha=0.7, rwidth=0.9)
-fr1.set_xlabel('Sequence Range')
-fr1.set_ylabel('Count')
-fr2.hist(S_P1_E2, BINS, label='count', alpha=0.7, rwidth=0.9)
-fr2.set_xlabel('Reading instance')
-fr3.hist(S_P1_E3, BINS, label='count', alpha=0.7, rwidth=0.9)
-fr3.set_xlabel('Reading instance')
-fr4.hist(S_P1_E4, BINS, label='count', alpha=0.7, rwidth=0.9)
-fr4.set_xlabel('Reading instance')
-fr5.hist(S_P1_P2, BINS, label='count', alpha=0.7, rwidth=0.9)
-fr5.set_xlabel('Reading instance')
 
-ev1.grid(True, axis='y', alpha=0.35)
-ev2.grid(True, axis='y', alpha=0.35)
-ev3.grid(True, axis='y', alpha=0.35)
-ev4.grid(True, axis='y', alpha=0.35)
-pha.grid(True, axis='y', alpha=0.35)
+for f in range(len(fAxes)):
+    title = 'Eaves ' + str(f + 1)
+    if f == 4:
+        title = 'In-body'
 
-fr1.grid(True, axis='y', alpha=0.35)
-fr2.grid(True, axis='y', alpha=0.35)
-fr3.grid(True, axis='y', alpha=0.35)
-fr4.grid(True, axis='y', alpha=0.35)
-fr5.grid(True, axis='y', alpha=0.35)
+    eAx = eAxes[f]
+
+    prr = 'PRR:' + str(round((len(PacketList[f]) / PACKETS), 3) * 100)[:4] + '%'
+    eAx.scatter(SequenceList[f], PacketList[f], s=SCATTER, label='from transmitter')
+    eAx.plot([np.mean(PacketList[f]) for _ in range(PACKETS)], label='rssi mean', linewidth=L_WIDTH)
+    eAx.set_xlim(0, PACKETS)
+    eAx.set_ylim(MIN_RSSI, MAX_RSSI)
+    eAx.set_title(title)
+    eAx.set_xlabel('Sequence number')
+    if f == 0:
+        eAx.set_ylabel('RSSI (dBm)')
+    eAx.text(0.3, 0.95, prr, transform=eAx.transAxes, fontsize=8, verticalalignment='center', bbox=props)
+
+    fAxes[f].hist(SequenceList[f], BINS, label='count', alpha=0.7, rwidth=0.9)
+    if f == 0:
+        fAxes[f].set_ylabel("Count")
+    fAxes[f].set_xlabel("Sequence Range")
+    fAxes[f].grid(True, axis='y', alpha=0.35)
+
+    eAx.grid(True, axis='y', alpha=0.35)
+    print("Done:", f)
 
 plt.savefig('results.png', dpi=300)
 
